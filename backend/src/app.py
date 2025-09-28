76420ce500c29dc142f8f8ad796131b84fbc60b0
mathip <116129737+MATHIP6@users.noreply.github.com>
2025-09-28 17:54:17 -0400

    Add CORS middleware

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+
+from fastapi.middleware.cors import CORSMiddleware
 from anime_sama_api import AnimeSama
 
 from src.controller import download, episodes, downloads
@@ -7,6 +9,13 @@ from .controller import search
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(index.router)
 app.include_router(search.router)
